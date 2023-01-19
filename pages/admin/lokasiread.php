a4688d842357eebbc74e94b8f716d74d8e49ab11
akbar3860 <kasur386@gmail.com>
2023-01-19 10:10:37 +0800

    Menampilkan data lokasi

--- pages/admin/lokasiread.php
+++ pages/admin/lokasiread.php
@@ -42,7 +42,6 @@
                 </tfoot>                
                 <tbody>
                     <?php
-                        include_once "database/database.php"; 
                         $database = new Database();
                         $db = $database->getConnection();
 
@@ -55,13 +54,13 @@
                         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                     ?>
                     <tr>
-                        <td><?= $no++ ?></td>
-                        <td><?= $row['nama_lokasi'] ?></td>
+                        <td><?php echo $no++ ?></td>
+                        <td><?php echo $row['nama_lokasi'] ?></td>
                         <td>
-                            <a href="?page=lokasiupdate&id=<?= $row['id'] ?>" class="btn btn-primary btn-sm mr-1">
+                            <a href="?page=lokasiupdate&id=<?php echo $row['id'] ?>" class="btn btn-primary btn-sm mr-1">
                                 <i class="fa fa-edit"></i> Ubah
                             </a>
-                            <a href="?page=lokasidelete&id=<?= $row['id'] ?>" class="btn btn-danger btn-sm mr-1" onclick="javascript: return confirm('Konfirmasi data akan dihapus?);">
+                            <a href="?page=lokasidelete&id=<?php echo $row['id'] ?>" class="btn btn-danger btn-sm" onClick="javascript: return confirm('Konfirmasi data akan dihapus?');">
                                 <i class="fa fa-trash"></i> Hapus
                             </a>
                         </td>
